698e2be1833657a873d629dbb358735066f412e9
seongminnpark <seongmi1@andrew.cmu.edu>
2016-12-22 00:55:18 +0900

    Read nfc tag in alarmActvity

--- app/src/main/java/com/main/seongmin/nfcalarm/AlarmActivity.java
+++ app/src/main/java/com/main/seongmin/nfcalarm/AlarmActivity.java
@@ -1,14 +1,71 @@
 package com.main.seongmin.nfcalarm;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by seongmin on 12/21/16.
  */
 public class AlarmActivity extends AppCompatActivity {
+
+    private NfcAdapter nfcAdapter;
+    private TextView alarmTextView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+
+        if (nfcAdapter == null || !nfcAdapter.isEnabled()) {
+            return;
+        }
+
+
     }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        setIntent(intent);
+        String action = intent.getAction();
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (tag == null) {
+                alarmTextView.setText("tag null");
+            } else {
+                alarmTextView.setText(tag.toString());
+            }
+        }
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        Intent intent = getIntent();
+//        String action = intent.getAction();
+//        alarmTextView = (TextView) findViewById(R.id.alarmText);
+//
+//        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+//            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//            if (tag == null) {
+//                alarmTextView.setText("tag null");
+//            } else {
+//                System.out.println("Received!");
+//                System.out.println(tag.toString());
+//                alarmTextView.setText("ffff" + tag.toString());
+//            }
+//        }
+//    }
+
 }
