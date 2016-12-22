d2f673d5c8740ee9e8865feec7315428506a102b
seongminnpark <seongmi1@andrew.cmu.edu>
2016-12-22 16:15:23 +0900

    Get rid of title bar

--- app/src/main/java/com/main/seongmin/nfcalarm/MainActivity.java
+++ app/src/main/java/com/main/seongmin/nfcalarm/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 /**
  * Created by seongmin on 12/22/16.
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         // Tabs setup.
