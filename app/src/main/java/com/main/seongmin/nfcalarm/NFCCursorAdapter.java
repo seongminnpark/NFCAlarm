b602a27cf0cd82be85f8c295893dfb4fad51ed3a
seongminnpark <seongmi1@andrew.cmu.edu>
2016-12-25 13:28:46 +0900

    Implement NFC deletion

--- app/src/main/java/com/main/seongmin/nfcalarm/NFCCursorAdapter.java
+++ app/src/main/java/com/main/seongmin/nfcalarm/NFCCursorAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /**
@@ -24,12 +25,23 @@ public class NFCCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView nfcItem = (TextView) view.findViewById(R.id.item_nfc);
+        TextView nfcItemName = (TextView) view.findViewById(R.id.itemNFCName);
+        ImageButton nfcItemDelete = (ImageButton) view.findViewById(R.id.itemNFCDelete);
 
+        final String nfcId = cursor.getString(cursor.getColumnIndexOrThrow(NFCContract.NFCEntry._ID));
         String uid = cursor.getString(cursor.getColumnIndexOrThrow(NFCContract.NFCEntry.COLUMN_NAME_UID));
         String name = cursor.getString(cursor.getColumnIndexOrThrow(NFCContract.NFCEntry.COLUMN_NAME_NAME));
 
-        nfcItem.setText(name + "   " + uid);
+        nfcItemName.setText(name + "   " + uid);
+
+        // Delete button setup.
+        nfcItemDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.dbHelper.deleteNFC(nfcId);
+                MainActivity.nfcCursorAdapter.refreshNFCList(MainActivity.dbHelper.loadNFCs());
+            }
+        });
     }
 
     public void refreshNFCList(Cursor cursor) {
